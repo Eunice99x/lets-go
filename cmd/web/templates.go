ad9575ddfe6a808414b45128125d06d19b9dc0c0
Younes Outerbah <younesouterbah1@gmail.com>
2024-07-26 22:37:31 +0100

    done

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -21,7 +21,10 @@ type templateData struct {
 
 // why exactly this date? {{ 02 Jan 2006 at 15:04 }} even copilot suggests to me
 func humanDate(t time.Time) string {
-	return t.Format("02 Jan 2006 at 15:04")
+	if t.IsZero() {
+		return ""
+	}
+	return t.UTC().Format("02 Jan 2006 at 15:04")
 }
 
 var functions = template.FuncMap{
---
+++ cmd/web/templates_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/eunice99x/lets-go/internal/assert"
+	"testing"
+	"time"
+)
+
+func TestHumanDate(t *testing.T) {
+	test := []struct {
+		name string
+		tm   time.Time
+		want string
+	}{
+		{
+			name: "UTC",
+			tm:   time.Date(2023, 3, 17, 10, 15, 0, 0, time.UTC),
+			want: "17 Mar 2023 at 10:15",
+		},
+		{
+			name: "Empty",
+			tm:   time.Time{},
+			want: "",
+		},
+		{
+			name: "CET",
+			tm:   time.Date(2023, 3, 17, 10, 15, 0, 0, time.FixedZone("CET", 1*60*60)),
+			want: "17 Mar 2023 at 09:15",
+		},
+	}
+
+	for _, tt := range test {
+		hd := humanDate(tt.tm)
+
+		assert.Equal(t, hd, tt.want)
+	}
+}
